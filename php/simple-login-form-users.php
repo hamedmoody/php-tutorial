c65f8e7470194442bbd943f8a40aa0ca74d1299a
Hamed Moodi <hamedmoodi2011@gmail.com>
2024-06-23 12:23:50 +0330

    Add users array

---
+++ php/simple-login-form-users.php
@@ -0,0 +1,63 @@
+<?php
+return [
+    [
+        'username'  => 'alimoodi',
+        'password'  => '5123688754',
+        'name'      => 'علی مودی',
+        'avatar'    => 'images/avatar-01.jpg',
+    ],
+    [
+        'username'  => 'rezarazavi',
+        'password'  => '847789651',
+        'name'      => 'رضا رضوی',
+        'avatar'    => 'images/avatar-02.jpg',
+    ],
+    [
+        'username'  => 'mojtaba',
+        'password'  => '9874552',
+        'name'      => 'مجتبی تهرانی',
+        'avatar'    => 'images/avatar-03.jpg',
+    ],
+    [
+        'username'  => 'morteza',
+        'password'  => '899986330',
+        'name'      => 'مرتضی طالب زاده',
+        'avatar'    => 'images/avatar-04.jpg',
+    ],
+    [
+        'username'  => 'armin',
+        'password'  => '963852741',
+        'name'      => 'آرمین مختاری',
+        'avatar'    => 'images/avatar-05.jpg',
+    ],
+    [
+        'username'  => 'rasoolamini',
+        'password'  => '21000365',
+        'name'      => 'رسول امینی',
+        'avatar'    => 'images/avatar-06.jpg',
+    ],
+    [
+        'username'  => 'mostafa',
+        'password'  => '7777896663',
+        'name'      => 'مصطفی محمودی',
+        'avatar'    => 'images/avatar-07.jpg',
+    ],
+    [
+        'username'  => 'jenifer',
+        'password'  => '1200125321',
+        'name'      => 'جنیفر',
+        'avatar'    => 'images/avatar-08.jpg',
+    ],
+    [
+        'username'  => 'liza',
+        'password'  => '8896669874',
+        'name'      => 'لیزا',
+        'avatar'    => 'images/avatar-09.jpg',
+    ],
+    [
+        'username'  => 'sara',
+        'password'  => '774582336',
+        'name'      => 'سارا',
+        'avatar'    => 'images/avatar-10.jpg',
+    ],
+];
