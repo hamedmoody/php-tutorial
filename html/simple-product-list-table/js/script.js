bd05034dc1f76ff9d148367bf6180ce054a964dd
Hamed Moodi <hamedmoodi2011@gmail.com>
2024-08-30 10:08:51 +0330

    Disable Thumbnail preview in CRUD project

--- html/simple-product-list-table/js/script.js
+++ html/simple-product-list-table/js/script.js
@@ -1,13 +1,14 @@
 jQuery(document).ready(function( $ ){
-    $('#thumbnail').change( function(e){
+
+    /*$('#thumbnail').change( function(e){
         e.preventDefault();
         if (e.target.files && e.target.files[0]) {
             const reader = new FileReader();
             reader.onload = function (e) {
                 $('.thumbnail-preview').attr( 'src', e.target.result );
-                $('.thumbnail').addClass('contain-image');
             }
             reader.readAsDataURL( e.target.files[0] );
         }
-    } );
+    } );*/
+
 });
