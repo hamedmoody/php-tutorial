76610888f47284302879779b68fe7113e7ffebe1
Hamed Moodi <hamedmoodi2011@gmail.com>
2024-12-04 20:26:21 +0330

    Complete Croppie test

--- html/music-website/js/script.js
+++ html/music-website/js/script.js
@@ -1,5 +1,9 @@
 jQuery(document).ready( function( $ ){
 
+    $('.close-croppie-modal').click( function(){
+        $('#cropper-wrapper').removeClass('show');
+    } );
+
 } );
 
 jQuery('.select2').select2();
