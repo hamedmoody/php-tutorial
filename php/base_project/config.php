bf3e10e5ab147e25db58291411692a5396cb39ed
Hamed Moodi <hamedmoodi2011@gmail.com>
2025-01-30 14:09:52 +0330

    [ADD] Add API Needed Function

---
+++ php/base_project/config.php
@@ -0,0 +1,9 @@
+<?php
+define( 'SITE_URL'          , 'https://musicjooyar.test/' );
+
+define( 'SITE_TIMEZONE'     , 'Asia/Tehran' );
+
+define( 'DB_NAME'           , 'musicjooyar' );
+define( 'DB_HOST'           , 'localhost' );
+define( 'DB_USER'           , 'root' );
+define( 'DB_PASSWORD'       , '' );
