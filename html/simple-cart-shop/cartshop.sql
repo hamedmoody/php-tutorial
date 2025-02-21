06e68fa491c81bea94b492223f15c6112f84cf8a
Hamed Moodi <hamedmoodi2011@gmail.com>
2025-02-21 14:56:45 +0330

    [ADD] Add countdown

--- html/simple-cart-shop/cartshop.sql
+++ html/simple-cart-shop/cartshop.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:3306
--- Generation Time: Feb 10, 2025 at 03:01 PM
+-- Generation Time: Feb 21, 2025 at 11:26 AM
 -- Server version: 8.0.30
 -- PHP Version: 8.1.10
 
--- html/simple-cart-shop/js/script.js
+++ html/simple-cart-shop/js/script.js
@@ -1,5 +1,29 @@
 jQuery(document).ready( function( $ ){
 
+    function setup_countdown(){
+        $('.countdown').each(function(){
+            let time = $(this).data('date');
+            let ts  = Date.parse( time ) / 1000;
+            let now_ts  = Math.floor( Date.now() / 1000 );
+            let remain  = ts - now_ts;
+            if( remain >= 0 ){
 
+                let days    = Math.floor( remain / 86400 );
+                let hours   = Math.floor( ( remain % 86400 ) / 3600 );
+                let minutes = Math.floor( ( remain % 3600 ) / 60 );
+                let seconds = remain % 60;
+
+                let result = '';
+                result+= days ? days.toString().padStart(2,'0') + ' روز و ' : '';
+                result+= `${hours.toString().padStart(2,'0')}:${minutes.toString().padStart(2,'0')}:${seconds.toString().padStart(2,'0')} ساعت`;
+
+                $(this).text( result );
+
+            }
+        });
+        setTimeout( setup_countdown, 1000 );
+    }
+
+    setup_countdown( );
 
 } );
