f463ad6f0f7d08508e5a0456033c1db6a3025d24
Hamed Moodi <hamedmoodi2011@gmail.com>
2025-06-10 21:54:29 +0330

    [ADD] Add base for oop model

---
+++ php/base_project_oop_model/config.php
@@ -0,0 +1,7 @@
+<?php
+define( 'SITE_TIMEZONE'     , 'Asia/Tehran' );
+
+define( 'DB_NAME'           , 'musicjooyar' );
+define( 'DB_HOST'           , 'localhost' );
+define( 'DB_USER'           , 'root' );
+define( 'DB_PASSWORD'       , '' );
---
+++ php/base_project_oop_model/init.php
@@ -0,0 +1,10 @@
+<?php
+include( 'config.php' );
+include( 'function-database.php' );
+include( 'database.php' );
+date_default_timezone_set( TIME_ZONE );
+
+/**
+ * Your Code Here
+ */
+include( 'Course.php' );
